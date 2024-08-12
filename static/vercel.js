4e1efa31941db2e4e44c750b79405157fcee6ace
hostelsforgeck <167711044+hostelsforgeck@users.noreply.github.com>
2024-08-12 20:10:03 +0530

    Delete static/vercel.js

--- static/vercel.js
+++
@@ -1,9 +0,0 @@
-// static/js/script.js
-window.va = window.va || function () {
-    (window.vaq = window.vaq || []).push(arguments);
-};
-
-window.si = window.si || function () {
-     (window.siq = window.siq || []).push(arguments);
-};
-
